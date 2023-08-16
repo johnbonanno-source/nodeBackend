d18c4c74ff828c59b09b384f03a885523d799f65
Ryankonesky <Ryankonesky1@gmail.com>
2023-08-15 20:09:50 -0600

    this is the node backend

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
+
+const app = express();
+
+const DUMMY_CHANGE_BALANCE = [1,2]; // not a database, just some in-memory storage for now which resets when we terminate the server
+
+const usersTable = [{
+  id: uuid(),
+  username: "john",
+  password: "p",
+}]; // MOCK DATA
+
+app.use(bodyParser.json());
+
+// CORS Headers => Required for cross-origin/ cross-server communication
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.get('/balance-history', (req, res, next) => {
+  res.status(200).json({ products: DUMMY_CHANGE_BALANCE });
+});
+
+app.get('/login', (req, res, next) => {
+
+  const {username} = req.body;
+  console.log(typeof(usersTable));
+  usersTable.forEach(user => {
+    console.log(user);
+    console.log(user["username"]);
+    //if(user["username"]===username){
+      res.status(200).json({ users: user });
+    //}
+    return res.status(422).json({
+      message: 'This code is bunk'
+    });
+  });
+});
+
+// let url = `http://localhost:5000/${usernameValue}`;
+// response.data.username === usernameValue && response.data.password===passwordValue
+
+app.post('/change-balance', (req, res, next) => {
+  const { amount } = req.body;
+
+  if (!amount || amount <= 0) {
+    return res.status(422).json({
+      message: 'Invalid input, please enter a valid amount.'
+    });
+  }
+
+  const changeBalance = {
+    id: uuid(),
+    changeType,
+    amount,
+    newBalance,
+    previousBalance,
+    date
+  };
+
+  //DUMMY_CHANGE_BALANCE.push(changeBalance);
+
+  res
+    .status(201)
+    .json({ message: 'Cash succesfully deposit.', balance: changeBalance });
+});
+
+app.listen(2000); // start Node + Express server on port 5000
+
+//Withdrawl or deposit, amount, New balance, previous balance, date of change
