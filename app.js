d1f9b21e7203b88a76b931552785c667553b991b
johnbonanno-source <john.bon991@gmail.com>
2023-08-29 17:19:26 -0600

    add logout route

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
-const connectDatabase = require('./db');
-const routes = require('./routes');
+const cors = require("cors");
+const connectDatabase = require("./db");
+const routes = require("./routes");
+const crypto = require("crypto");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', routes);
+const generateSecretKey = () => {
+  return crypto.randomBytes(32).toString("hex");
+};
+
+const secretKey = generateSecretKey();
+
+app.use((req, res, next) => {
+  req.secretKey = secretKey;
+  next();
+});
+
+app.use("/", routes);
 
 connectDatabase().then(() => {
   app.listen(2001, () => {
